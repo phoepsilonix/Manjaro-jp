f5bb660139ad99550fe5928ba9f43d3c8065e0ce
Masato Toyoshima <phoepsilonix@gmail.com>
2022-06-16 15:54:19 +0900

    fix

--- buildiso3.sh
+++ buildiso3.sh
@@ -54,19 +54,19 @@ do
 	data=(${edition[@]})
 	ed=${data[1]}
         echo "build pre-image"
-        echo "buildiso -d xz -f -k $kernel -p $ed -x $gkey -t ./tmp/tmp -r ./tmp/mytmp/"
-        #buildiso -d xz -f -k $kernel -p $ed -x $gkey -t $usb/tmp/iso 
-        buildiso -d xz -f -k $kernel -p $ed -x $gkey -t ./tmp/tmp -r ./tmp/mytmp
+        echo "buildiso -d xz -f -k $kernel -p $ed -x $gkey -t ~/tmp/tmp -r ~/tmp/mytmp/"
+        #buildiso -d xz -f -k $kernel -p $ed -x $gkey -t ~/tmp/tmp -r ~/tmp/mytmp
+        #buildiso -d xz -f -k $kernel -p $ed -x $gkey -t ~/tmp/tmp/ 
         echo "build iso"
-        echo "buildiso -d xz -f -k $kernel -p $ed -zc $gkey -t ./tmp/tmp -r ./tmp/mytmp"
-        #buildiso -d xz -f -k $kernel -p $ed -zc $gkey -t $usb/tmp/iso 
-        buildiso -d xz -f -k $kernel -p $ed -zc $gkey -t ./tmp/tmp -r ./tmp/mytmp
+        echo "buildiso -d xz -f -k $kernel -p $ed -zc $gkey -t ~/tmp/tmp -r ~/tmp/mytmp"
+        buildiso -d xz -f -k $kernel -p $ed -zc $gkey -t ~/tmp/tmp -r ~/tmp/mytmp
+        #buildiso -d xz -f -k $kernel -p $ed -zc $gkey -t ~/tmp/tmp/ 
 done
 
 echo "Move iso files to Artifacts folder"
 #sudo chown -R phoepsilonix:phoepsilonix $usb/tmp/iso/
 sync
-find ./.tmp/tmp ./.tmp/mytmp -type f -name "*.iso" | xargs -I{} mv {} $usb/artifacts/ && sync
+find ~/tmp/tmp ~/tmp/mytmp -type f -name "*.iso" | xargs -I{} mv {} $usb/artifacts/ && sync
 rsync -avn $usb/artifacts/*.iso $artifacts/ && sync
 sync
 
