f6733ab55654cad8b4bfb50980584d5cee6399c7
Masato Toyoshima <phoepsilonix@gmail.com>
2022-06-26 22:36:37 +0900

    fix

--- buildiso1.sh
+++ buildiso1.sh
@@ -20,8 +20,8 @@ pkg3=Packages-Root
 
 # エディション指定
 editions=(
-#        "manjaro gnome"
-#	"manjaro kde"
+        "manjaro gnome"
+	"manjaro kde"
 	"manjaro xfce"
 #	"community cinnamon" 
 #       "community mate"
@@ -59,7 +59,7 @@ do
 	ed=${data[1]}
         echo "build pre-image"
         echo "buildiso -d xz -f -k $kernel -p $ed -x $gkey -t $usb/tmp/iso -r $usb/tmp/build"
- #       buildiso -d xz -f -k $kernel -p $ed -x $gkey -t $usb/tmp/iso 
+        buildiso -d xz -f -k $kernel -p $ed -x $gkey -t $usb/tmp/iso 
 #        buildiso -d xz -f -k $kernel -p $ed -x $gkey -t $usb/tmp/iso -r $usb/tmp/build
         echo "build iso"
         echo "buildiso -d xz -f -k $kernel -p $ed -zc $gkey -t $usb/tmp/iso -r $usb/tmp/build"
