b8d740362f1b79f6d86f8d97738e02ac03e94af1
Masato Toyoshima <phoepsilonix@gmail.com>
2022-06-30 15:14:37 +0900

    sign.sh

---
+++ Packages/sign.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+repo_key=$(cat ~/.gnupg/sign.txt)
+curdir=${PWD##*/}
+for m in $(cat ../extramodules.txt)
+do
+        cd $m;
+# 署名がないパッケージに署名をする
+for f in *.zst 
+do
+	[[ ! -f "$f.sig" ]] && { echo "gpg sign: $f" ; gpg --default-key $repo_key -v -b $f; }
+done
+
+# パッケージの署名の検証
+for f in *.zst.sig 
+do
+	echo "$f ${f%.*} gpg verify"
+	gpg -v --default-key $repo_key --verify $f ${f%.*} || { echo "pkg verify error" ; exit 1; }
+done
+        cd ..;
+done
+ 
+exit 0
+
