8703e3042888f1ca80928f7a09c26cf3b9b6d79c
Masato Toyoshima <phoepsilonix@gmail.com>
2022-07-16 15:26:05 +0900

    sourceforge rsync

--- SF-rsync.sh
+++ SF-rsync.sh
@@ -5,13 +5,13 @@ repo_dir=artifacts
 # SourceForge
 echo "SourceForge"
 # iso
-rsync -aLvnP --delete $repo_dir/ phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
+rsync -LtgoDvnPd --delete $repo_dir/ phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
 
 # torrentファイルよりも先にisoをアップロードする。
 rsync -LtgoDvP --progress $repo_dir/*.iso phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
 
 # artifactsフォルダのみ。SFはファイル名にコロンを許容しないので、manjaro-jpはバックアップしない。
-rsync -LtgoDvP --progress --delete $repo_dir/ phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
+rsync -LtgoDvPd --progress --delete $repo_dir/* phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
 
 rsync -aLvP --progress README.md README.ja.md README.en.md phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
 rsync -aLvP --progress ./index.html ./index.en.html ./index.ja.html phoepsilonix@web.sourceforge.net:/home/project-web/manjaro-jp/htdocs/
