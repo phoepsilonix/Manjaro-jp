09e82e6c078c40d97274769ccc3bcac1a94381df
Masato Toyoshima <phoepsilonix@gmail.com>
2022-08-06 14:13:37 +0900

    repo-update

--- Packages/repo.sh
+++ Packages/repo.sh
@@ -4,5 +4,6 @@ curdir=${PWD##*/}
 repodir=~/gitlab/Manjaro-jp/artifacts/manjaro-jp/
 
 find . -name "linux518-*.zst" -o -name "linux518-*.zst.sig" -type f | xargs -I{} mv {} $repodir
+find . -name "linux515-*.zst" -o -name "linux515-*.zst.sig" -type f | xargs -I{} mv {} $repodir
 
 exit 0
