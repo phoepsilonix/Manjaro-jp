fcb9d536a51d92b950682be83dccac94a89ab492
Masato Toyoshima <phoepsilonix@gmail.com>
2022-09-10 22:24:07 +0900

    SF-rsync.sh fix

--- SF-rsync.sh
+++ SF-rsync.sh
@@ -11,7 +11,7 @@ rsync -LtgoDvnPd --delete $repo_dir/ phoepsilonix@frs.sourceforge.net:/home/pfs/
 rsync -LtgoDvP --progress $repo_dir/*.iso phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
 
 # artifactsフォルダのみ。SFはファイル名にコロンを許容しないので、manjaro-jpはバックアップしない。
-rsync -LtgoDvPd --progress --delete $repo_dir/* phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
+rsync -LtgoDvPd --progress --delete $repo_dir/ phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
 
 rsync -aLvP --progress README.md README.ja.md README.en.md phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
 rsync -aLvP --progress ./index.html ./index.en.html ./index.ja.html phoepsilonix@web.sourceforge.net:/home/project-web/manjaro-jp/htdocs/
