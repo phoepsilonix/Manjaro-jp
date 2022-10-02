cf0f91c2e728f722bf79f232b30b691021750ac2
Masato Toyoshima <phoepsilonix@gmail.com>
2022-10-03 00:17:34 +0900

    alert

---
+++ alert.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+P=$@
+SOUND=/usr/share/sounds/freedesktop/stereo/phone-outgoing-calling.oga
+
+echo $P
+PID=$(pidof -x $P|sed 's/\([0-9]*\)/--pid=\0/g')
+echo $PID
+
+tail -f /dev/null $PID && (play $SOUND ; gnome-text-editor )
+
+exit 0
