0df2c4a439bc0ddc6e540f0794f9871c1459005e
Masato Toyoshima <phoepsilonix@gmail.com>
2022-10-03 02:36:31 +0900

    volume

--- alert.sh
+++ alert.sh
@@ -7,6 +7,6 @@ echo $P
 PID=$(pidof -x $P|sed 's/\([0-9]*\)/--pid=\0/g')
 echo $PID
 
-tail -f /dev/null $PID && (play $SOUND ; gnome-text-editor )
+tail -f /dev/null $PID && (play -v3 $SOUND ; gnome-text-editor )
 
 exit 0
