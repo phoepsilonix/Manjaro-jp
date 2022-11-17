41b389075224f4b30658c6149949ee00ad7aad53
Masato Toyoshima <phoepsilonix@gmail.com>
2022-11-17 12:22:34 +0900

    makepkg

--- Packages/makepkg.sh
+++ Packages/makepkg.sh
@@ -1,14 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
+export HISTIGNORE="expect*";
 export IGNORE_CC_MISMATCH=1
+password=$(cat ~/.ssh/gpg-passphrase)
 
 curdir=${PWD##*/}
 for m in $(cat ../extramodules.txt)
 do
         cd $m;
-        cat ~/.ssh/gpg-passphrase|sudo -S pwd > /dev/null
-        yes | makepkg -sCc
+#        cat ~/.ssh/gpg-passphrase|sudo -S pwd > /dev/null
+expect -c "
+  set timeout -1
+  spawn makepkg -sCc
+  expect {
+        eof { exit 0 }
+        \"password for\" {
+                send -- \"$password\r\"
+                exp_continue
+        }
+        \"y/N\" {
+                send \"y\r\"
+                exp_continue
+        }
+        \"Y/n\" {
+                send \"y\r\"
+                exp_continue
+        }
+        exp_continue
+  }
+"
+#        makepkg -sCc --noconfirm
         cd ..;
 done
  
 exit 0
+
