84af5bf49c23e6282d1161c7af4c82cc2b6d582c
Masato Toyoshima <phoepsilonix@gmail.com>
2022-11-30 22:32:31 +0900

    fix

--- OSDN-SF-mirror.sh
+++ OSDN-SF-mirror.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 #set -eux
 
+. keychain.sh
+
 repo_dir=./artifacts/
 cd $repo_dir;
 
 mkdir -p $repo_dir/manjaro-jp/
-
-###### expectによる、履歴を残さない
-export HISTIGNORE="expect*";
-
-###### スクリプト終了時には、keychainをクリアして、ssh-agentを停止する。
-trap "
-keychain --clear
-keychain -k mine
-" EXIT
-
-###### sshのパスフレーズを復号化する
-password=$(gpg --passphrase-file $gpg_pass --batch --pinentry-mode=loopback -dq $ssh_pass)
-
-###### expectでパスフレーズ入力を自動化
-expect << EOF
-  spawn keychain --agents ssh --eval id_ed25519
-  expect "* passphrase *:" {
-        send "$password\r"
-  }
-  expect eof
-EOF
 
 ###### このスクリプト内で、有効化させる。
 eval `keychain --agents ssh --eval id_ed25519 2>/dev/null`
