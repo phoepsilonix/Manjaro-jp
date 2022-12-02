aa5a89961ec83e5e5c375f67885e0b208e28074a
Masato Toyoshima <phoepsilonix@gmail.com>
2022-12-02 23:27:32 +0900

    fix

--- line-notify.sh
+++ line-notify.sh
@@ -1,16 +1,9 @@
 #! /bin/sh
 
 P=$@
-SOUND=/usr/share/sounds/freedesktop/stereo/phone-outgoing-calling.oga
-
-PID=$(pidof -x $P|sed 's/\([0-9]*\)/--pid=\0/g')
-echo $P $PID
 token=$(cat ~/.ssh/line-token)
 
-tail -f /dev/null $PID && \
-        curl -X POST -H "Authorization: Bearer $token" -F message="$P done" https://notify-api.line.me/api/notify || \
-        curl -X POST -H "Authorization: Bearer $token" -F message="$P error" https://notify-api.line.me/api/notify
+curl -X POST -H "Authorization: Bearer $token" -F message="$P" https://notify-api.line.me/api/notify
 
-echo
 exit 0
 
