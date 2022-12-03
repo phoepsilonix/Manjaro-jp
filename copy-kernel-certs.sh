4cc8fdc95affb53456ee5d875dd34792927dc233
Masato Toyoshima <phoepsilonix@gmail.com>
2022-12-03 18:44:48 +0900

    fix

---
+++ copy-kernel-certs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+base=$(grep "_basekernel=" PKGBUILD)
+ver1=$(grep "pkgver=" PKGBUILD)
+ver2=$(grep "pkgrel=" PKGBUILD)
+ver=${ver1##pkgver=}-${ver2##pkgrel=}-MANJARO
+base=${base##_basekernel=}
+dir=src/linux-${base}/certs/
+
+for f in $dir/signing_key.*
+do
+        f2=${f##*/}
+        #echo $f ${f%\.*}-$ver.${f##*\.}
+        echo cp $f ${f2%\.*}-$ver.${f2##*\.}
+        cp $f ${f2%\.*}-$ver.${f2##*\.}
+done
+
