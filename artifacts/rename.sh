5bff824632b6ee939ff7b56b0260738adbcfabdf
Masato Toyoshima <phoepsilonix@gmail.com>
2022-12-08 13:00:16 +0000

    fix

--- artifacts/rename.sh
+++ artifacts/rename.sh
@@ -1,6 +1,8 @@
 #/usr/bin/env sh
 
 #cd artifacts;
+browser=
+browser=floorp
 browser=firefox
 
 for f in manjaro-*.iso ;
@@ -9,7 +11,7 @@ do
                 continue;
         else
                 f2=$(echo $f|sed s/^manjaro/manjaro-jp/);
-                f2=$(echo $f2|sed s/\.iso/-${browser}.iso/);
+                [[ "$browser" != "" ]] && f2=$(echo $f2|sed s/\.iso/-${browser}.iso/);
         fi
         echo $f $f2;
         mv $f $f2;
