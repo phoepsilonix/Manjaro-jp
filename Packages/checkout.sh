a2b328885a41d995fdf68251566a069273f94be6
Masato Toyoshima <phoepsilonix@gmail.com>
2022-12-22 13:22:29 +0000

    submodule

--- Packages/checkout.sh
+++ Packages/checkout.sh
@@ -14,9 +14,9 @@ sudo chown -R phoepsilonix:phoepsilonix $exdir
 #cd ../tp_smapi
 #git diff | patch -R -i -
 #cd ..
-cd tp_smapi
-rm tp_smapi-clang.patch
-cd ..
+#cd tp_smapi
+#rm tp_smapi-clang.patch
+#cd ..
 
 for m in $(cat ../extramodules.txt)
 do
@@ -26,10 +26,10 @@ do
 
         # masterブランチをchekoutして、pullでリモートの最新版を取得する
         cd $m;
-        git switch -f master
         git diff --binary HEAD | git apply --check --stat --apply --allow-empty -R -
+        git switch -f master
         git checkout master;
-        git pull origin master;
+        git pull origin master || (rm ./* -rf && git pull origin master)
         for patch in ~/gitlab/Manjaro-jp/patches/${m}*\.patch ~/gitlab/Manjaro-jp/patches/*${m}\.patch
         do
                 if [[ -e $patch ]];then
@@ -40,9 +40,9 @@ do
         cd ..;
 done
  
-cd r8168
+#cd r8168
 #patch -i /home/phoepsilonix/gitlab/Manjaro-jp/patches/r8168-extramodules.patch
-updpkgsums
+#updpkgsums
 #cd ../tp_smapi
 #rm tp_smapi-clang.patch
 #patch -i /home/phoepsilonix/gitlab/Manjaro-jp/patches/tp_smapi-build-with-clang.patch
