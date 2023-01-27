43b886709ff86ba451e8aad5cd873828666e83d8
Masato Toyoshima <phoepsilonix@gmail.com>
2023-01-28 02:24:27 +0900

    rsync sig file

--- OSDN-SF-mirror.sh
+++ OSDN-SF-mirror.sh
@@ -16,14 +16,15 @@ eval `keychain --agents ssh --eval id_ed25519 2>/dev/null`
 echo "OSDN"
 eval `keychain --agents ssh --eval id_ed25519`
 rsync -avPL --size-only --no-perms --delete ./ phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
+rsync -avPLc --no-perms --delete ./*.sig phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
 rsync -avPLz --no-perms ./*.css ./*.js index.html index.ja.html index.en.html README.ja.html README.en.html phoepsilonix@shell.osdn.net:/home/groups/m/ma/manjaro-jp/htdocs
 
 # SourceForge
 echo "SourceForge"
 # iso
 eval `keychain --agents ssh --eval id_ed25519`
-rsync -aLvPn --size-only --delete --exclude=manjaro-jp/* --exclude=*\.html --exclude=*\.css ./ phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
 rsync -aLvP --size-only --delete --exclude=manjaro-jp/* --exclude=*\.html --exclude=*\.css ./ phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
+rsync -aLvPc --delete --exclude=manjaro-jp/* --exclude=*\.html --exclude=*\.css ./*.sig phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
 rsync -avPLz ./*.css ./*.js ./index.html ./index.en.html ./index.ja.html ./README.ja.html ./README.en.html phoepsilonix@web.sourceforge.net:/home/project-web/manjaro-jp/htdocs/
 
 
