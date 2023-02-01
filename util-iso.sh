1ee80d7a1924d138de83296e323b982d3fe65d68
Masato Toyoshima <phoepsilonix@gmail.com>
2023-02-02 01:31:23 +0900

    floorp

--- util-iso.sh
+++ util-iso.sh
@@ -365,8 +365,8 @@ make_image_desktop() {
         if [[ "${profile}" != "architect" ]];then
                 systemd-nspawn -D ${path} flatpak remote-add  --system --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 	        # Browser 
-	        #systemd-nspawn -D ${path} flatpak install -y one.ablaze.floorp
-                systemd-nspawn -D ${path} flatpak install -y org.mozilla.firefox
+	        systemd-nspawn -D ${path} flatpak install -y one.ablaze.floorp
+                #systemd-nspawn -D ${path} flatpak install -y org.mozilla.firefox
                 systemd-nspawn -D ${path} flatpak install -y org.libreoffice.LibreOffice
         fi
         
