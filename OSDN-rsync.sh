aa95f2086d7989c10d9a361fb2a40500162a8f1e
Masato Toyoshima <phoepsilonix@gmail.com>
2023-02-06 21:34:13 +0900

    bt-seed test

--- OSDN-rsync.sh
+++ OSDN-rsync.sh
@@ -16,17 +16,17 @@ set -u     # Stop if an unbound variable is referenced
 # OSDNへアップデート
 echo "OSDN"
 #主にmanjaro-jp以下のファイルをアップロード
-rsync -avPL --no-perms --exclude=*.iso --exclude=\.* --exclude=*\.torrent $repo_dir/* phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
+#rsync -avPL --no-perms --exclude=*.iso --exclude=\.* --exclude=*\.torrent $repo_dir/* phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
 
 # isoファイルをアップロード
 # torrentファイルはisoのあとにアップロードさせる。
 eval `keychain --agents ssh --eval id_ed25519 2>/dev/null`
-rsync -avPL --size-only --no-perms --exclude=\.* --exclude=*\.torrent $repo_dir/* phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
+#rsync -avPL --size-only --no-perms --exclude=\.* --exclude=*\.torrent $repo_dir/* phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
 #rsync -ptgoLvP --no-perms --exclude=\.* $repo_dir/*.torrent phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
 
 # 残りをまとめて高速チェックでアップロード
 eval `keychain --agents ssh --eval id_ed25519 2>/dev/null`
-rsync -avPL --no-perms --size-only --exclude=\.*  $repo_dir/ phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
+#rsync -avPL --no-perms --size-only --exclude=\.*  $repo_dir/ phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
 eval `keychain --agents ssh --eval id_ed25519 2>/dev/null`
 rsync -avPLz --no-perms $repo_dir/README.md $repo_dir/README.en.md $repo_dir/README.ja.md phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/
 eval `keychain --agents ssh --eval id_ed25519 2>/dev/null`
---
+++ bt-test.sh
@@ -0,0 +1,1 @@
+aria2c -V --seed-ratio=0.0 artifacts/*.torrent -dartifacts/ --enable-dht6 --enable-dht  --bt-seed-unverified=true -j32
