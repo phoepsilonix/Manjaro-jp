9797a4dfaa8a943e6f9c178bc913c0b9fc30fb9a
Masato Toyoshima <phoepsilonix@gmail.com>
2023-02-09 17:07:30 +0900

    upload scripts

--- OSDN-SF-mirror.sh
+++ OSDN-SF-mirror.sh
@@ -15,9 +15,14 @@ eval `keychain --agents ssh --eval id_ed25519 2>/dev/null`
 # OSDNへアップデート
 echo "OSDN"
 eval `keychain --agents ssh --eval id_ed25519`
-rsync -avPL --size-only --no-perms --delete ./ phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
-rsync -avPLc --no-perms --delete ./*.sig phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
+#rsync -avPL --size-only --no-perms --delete ./ phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
+#rsync -avPLc --no-perms --delete ./*.sig phoepsilonix@storage.osdn.net:/storage/groups/m/ma/manjaro-jp/ || { echo "OSDN rsync error" ; exit 1 ; }
 rsync -avPLz --no-perms ./*.css ./*.js ./*.md index.html index.ja.html index.en.html README.ja.html README.en.html phoepsilonix@shell.osdn.net:/home/groups/m/ma/manjaro-jp/htdocs
+#OSDN web
+#OK:symlink
+eval `keychain --agents ssh --eval id_ed25519 2>/dev/null`
+rsync -avP --delete manjaro-jp/ phoepsilonix@shell.osdn.net:/home/groups/m/ma/manjaro-jp/htdocs/manjaro-jp/
+
 
 # SourceForge
 echo "SourceForge"
@@ -26,6 +31,11 @@ eval `keychain --agents ssh --eval id_ed25519`
 rsync -aLvP --size-only --delete --exclude=manjaro-jp/* --exclude=*\.html --exclude=*\.css ./ phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
 rsync -aLvPc --delete --exclude=manjaro-jp/* --exclude=*\.html --exclude=*\.css ./*.sig phoepsilonix@frs.sourceforge.net:/home/pfs/project/manjaro-jp/ || { echo "SF rsync error" ; exit 1 ; }
 rsync -avPLz ./*.css ./*.js ./*.md ./index.html ./index.en.html ./index.ja.html ./README.ja.html ./README.en.html phoepsilonix@web.sourceforge.net:/home/project-web/manjaro-jp/htdocs/
+#sourceforge web
+#X:symlink rsync -L
+eval `keychain --agents ssh --eval id_ed25519 2>/dev/null`
+rsync -avPL --delete manjaro-jp/ phoepsilonix@web.sourceforge.net:/home/project-web/manjaro-jp/htdocs/manjaro-jp/
+
 
 
 exit 0;
