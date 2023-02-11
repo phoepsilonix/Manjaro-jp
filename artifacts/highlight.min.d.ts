e819c7d9cd4d70699479bfaed0fd51d0840f100d
Masato Toyoshima <phoepsilonix@gmail.com>
2023-02-11 20:55:19 +0900

    .ts file

---
+++ artifacts/highlight.min.d.ts
@@ -0,0 +1,8 @@
+declare var __extends: any;
+declare var __spreadArray: any;
+/*!
+  Highlight.js v11.7.0 (git: 82688fad18)
+  (c) 2006-2022 undefined and other contributors
+  License: BSD-3-Clause
+ */
+declare var hljs: void;
