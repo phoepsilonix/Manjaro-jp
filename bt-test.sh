42a648b59bc866efbad8ef86cfe531f09745a4e9
Masato Toyoshima <phoepsilonix@gmail.com>
2023-02-11 23:24:12 +0900

    bt-test tracker

--- bt-test.sh
+++ bt-test.sh
@@ -1,2 +1,4 @@
+trackers=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/,/g' trackers_all.txt |sed -e 's/,$//')
+
 aria2c -V --seed-ratio=0.0 artifacts/*.torrent -dartifacts/ --enable-dht6 --enable-dht  --bt-seed-unverified=true -j32 \
---bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451/announce
+--bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451/announce,$trackers
