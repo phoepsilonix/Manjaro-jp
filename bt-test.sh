7b24fd199eb3c55f6f50eb5520e19b0b3176a909
Masato Toyoshima <phoepsilonix@gmail.com>
2023-02-11 22:49:42 +0900

    bt-test aria2c

--- bt-test.sh
+++ bt-test.sh
@@ -1,1 +1,2 @@
-aria2c -V --seed-ratio=0.0 artifacts/*.torrent -dartifacts/ --enable-dht6 --enable-dht  --bt-seed-unverified=true -j32
+aria2c -V --seed-ratio=0.0 artifacts/*.torrent -dartifacts/ --enable-dht6 --enable-dht  --bt-seed-unverified=true -j32 \
+--bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451
