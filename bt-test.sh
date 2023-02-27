593b83cfcdd5c4e0320da3b00a304009ee5b9444
Masato Toyoshima <phoepsilonix@gmail.com>
2023-02-27 14:05:28 +0900

    numlock state gnome

--- bt-test.sh
+++ bt-test.sh
@@ -2,5 +2,5 @@ trackers=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/,/g' trackers_all.txt |sed -e '
 
 #--bt-seed-unverified=true \
 aria2c -V --seed-ratio=0.0 artifacts/*.torrent -dartifacts/ --enable-dht6 --enable-dht -j200 \
-    --bt-seed-unverified \
+    --bt-seed-unverified=true \
 --bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451/announce,$trackers
