b2d4154c7cbb91d577a8866214f777765aec903d
Masato Toyoshima <phoepsilonix@gmail.com>
2023-03-07 10:14:54 +0900

    etc

--- restart.sh
+++ restart.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-gpg -dq ~/.ssh/pass.gpg|sudo -S pacman -Sy nginx-quic nginx-quic-mod-brotli git-lfs base-devel neovim clang lld llvm aria2 manjaro-tools-iso-git rsync sox --needed
-sudo pacman -Sy --needed paru fuse-overlayfs keychain expect 
+gpg -dq ~/.ssh/pass.gpg|sudo -S pacman -Sy git-lfs base-devel neovim clang lld llvm aria2 manjaro-tools-iso-git rsync sox paru fuse-overlayfs keychain expect h2o-git --needed
 
 mkdir -p ~/gitlab
 cd ~/gitlab
 [[ ! -e Manjaro-jp ]] && git clone git@gitlab.com:phoepsilonix/Manjaro-jp.git
 cd ~/gitlab/Manjaro-jp
-git submodule --init
+git submodule update --init
 
 sudo cp makepkg.conf /etc/
 cp makepkg.conf ~/.makepkg.conf
@@ -15,23 +14,23 @@ cp makepkg.conf ~/.makepkg.conf
 usb=/run/media/phoepsilonix/Ventoy
 [[ -e $usb/ ]] && rsync -avp $usb/artifacts/ ./artifacts/
 
-sudo cp nginx.conf /etc/nginx/
-sync
-sudo systemctl enable nginx
-sudo systemctl start nginx
+#sudo cp nginx.conf /etc/nginx/
+#sync
+#sudo systemctl enable nginx
+#sudo systemctl start nginx
 cp iso-profiles.conf ~/.config/manjaro-tools/
 #dein.vim
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+#curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 # For example, we just use `~/.cache/dein` as installation directory
-sh ./installer.sh ~/.cache/dein
-rm installer.sh
+#sh ./installer.sh ~/.cache/dein
+#rm installer.sh
 
 # iso-profiles buildiso
 mkdir -p ~/work
 cd ~/work
-git clone https://gitlab.manjaro.org/profiles-and-settings/iso-profiles.git iso-profiles
-git clone ssh://git@gitlab.manjaro.org:22277/packages/core/linux519.git
-git clone ssh://git@gitlab.manjaro.org:22277/packages/core/linux515.git
+git clone --depth 1 --no-single-branch https://gitlab.manjaro.org/profiles-and-settings/iso-profiles.git iso-profiles
+#git clone ssh://git@gitlab.manjaro.org:22277/packages/core/linux519.git
+#git clone ssh://git@gitlab.manjaro.org:22277/packages/core/linux515.git
 
 # end
 exit 0
