9527de0037b1ae2bfc4d886687c9385e7d678e09
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2023-10-11 17:33:37 +0900

    bt-test.sh

--- bt-test.sh
+++ bt-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-trackers=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/,/g' trackers_all.txt |sed -e 's/,$//')
+trackers=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/,/g' -e 's/,,/,/g' trackers_all.txt |sed -e 's/,$//')
 #MAGNET=""
 #TORRENT=$(ls artifacts/*.torrent |xargs -n1 echo -n " -T ")
 TORRENT=$(ls artifacts/*.torrent)
@@ -15,8 +15,8 @@ gpg -d ~/.ssh/pass.gpg | sudo -S choom -n -1000 -p $$
 #aria2c -V --seed-ratio=0.0 -c artifacts/*.torrent -dartifacts --enable-dht6 --enable-dht -j2 -x2 -s2 \
 aria2c -V --seed-ratio=0.0 -j200 -x2 -s2 -Z -P -c $TORRENT -dartifacts --enable-dht6 --enable-dht \
     --bt-seed-unverified=true \
---bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451/announce \
-#--bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451/announce,$trackers
+--bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451/announce,$trackers
+#--bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451/announce \
 
 #--dht-listen-addr6=2406:8c00:0:3428:133:18:241:73 \
     #--bt-seed-unverified=true \
