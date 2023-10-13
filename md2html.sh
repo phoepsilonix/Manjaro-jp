53221fa225d9a1ff453fc6651b948a4a9ea1dc56
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2023-10-13 14:44:23 +0900

    md2html pulldown-cmark options

--- md2html.sh
+++ md2html.sh
@@ -5,7 +5,7 @@ for f in ./*.*.md
 do
     f2=${f%.md}.html
     echo $f $f2
-    pulldown-cmark -TFSLPH $f > $f2
+    pulldown-cmark -TFSLPHM $f > $f2
     #pulldown-cmark -TFSLPH < $f > $f2
     #pulldown-cmark $f > $f2
 done
