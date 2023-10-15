261faa9c5d810f2f76eab42c9230b2b141d8f3d6
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2023-10-15 14:27:30 +0900

    bt-test.sh

--- bt-test.sh
+++ bt-test.sh
@@ -4,7 +4,9 @@ trackers=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/,/g' -e 's/,,/,/g' trackers_all
 #MAGNET=""
 #TORRENT=$(ls artifacts/*.torrent |xargs -n1 echo -n " -T ")
 TORRENT=$(ls artifacts/*.torrent)
-PARALLEL=$(($(ls artifacts/*.torrent|wc -l)*4))
+TORRENT2=$(ls artifacts/firefox/*.torrent)
+PARALLEL=$(($(ls artifacts/*.torrent |wc -l)*4))
+PARALLEL2=$(($(ls artifacts/firefox/*.torrent |wc -l)*4))
 #for f in $TORRENT
 #do
 #    MAGNET+=" $(python torrent2magnet/torrent2magnet.py $f 2>/dev/null)"
@@ -14,9 +16,12 @@ gpg -d ~/.ssh/pass.gpg | sudo -S choom -n -1000 -p $$
 #aria2c -V --seed-ratio=0.0 -c $MAGNET -T artifacts/*.torrent -dartifacts --enable-dht6 --enable-dht -j2 -x2 -s2 \
 #aria2c -V --seed-ratio=0.0 -T artifacts/*.torrent -dartifacts --enable-dht6 --enable-dht -j2 -x2 -s2 \
 #aria2c -V --seed-ratio=0.0 -c artifacts/*.torrent -dartifacts --enable-dht6 --enable-dht -j2 -x2 -s2 \
-aria2c -V --seed-ratio=0.0 -j$PARALLEL -x2 -s2 -Z -P -c $TORRENT -dartifacts --enable-dht6 --enable-dht \
+nohup aria2c -V --seed-ratio=0.0 -j$PARALLEL -x2 -s2 -Z -P -c $TORRENT -dartifacts --enable-dht6 --enable-dht \
     --bt-seed-unverified=true \
---bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451/announce,$trackers
+--bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451/announce,$trackers >/dev/null 2>&1 &
+nohup aria2c -V --seed-ratio=0.0 -j$PARALLEL2 -x2 -s2 -Z -P -c $TORRENT2 -dartifacts/firefox --enable-dht6 --enable-dht \
+    --bt-seed-unverified=true \
+--bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451/announce,$trackers >/dev/null 2>&1 &
 #--bt-tracker=udp://tracker.opentrackr.org:1337/announce,udp://tracker.torrent.eu.org:451/announce \
 
 #--dht-listen-addr6=2406:8c00:0:3428:133:18:241:73 \
