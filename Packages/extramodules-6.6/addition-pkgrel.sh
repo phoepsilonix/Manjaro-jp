2c352127d55d6bc01ec14cd47c26746c84ef38c8
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2024-01-06 16:36:46 +0900

    addition-pkgrel

---
+++ Packages/extramodules-6.6/addition-pkgrel.sh
@@ -0,0 +1,9 @@
+
+for m in $(cat extramodules.txt)
+do
+    f=$(ls ~/gitlab/Manjaro-jp/artifacts/manjaro-jp/linux66-$m*.zst);
+    pkgrel=$(echo $f|sed "s|.*-\(.*\)-x86_64.*|\1|")
+    pkgrel=$((pkgrel+1))
+    echo $m $pkgrel
+    sed "s|pkgrel=.*|pkgrel=${pkgrel}|" $m/PKGBUILD -i
+done
