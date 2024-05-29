9eb1eba61464a9cd686102c7859c7cde3aaf0fa8
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2024-05-29 15:30:58 +0900

    hardlink cp -al

--- hardlink.sh
+++ hardlink.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-rm ~/www/artifacts -rf
-for f in $(find artifacts/ -type d);do mkdir -p ~/www/$f; files=$(ls -Fd $f/* | grep -v "/$"| sed 's/[*@]$//');ln $files ~/www/$f/ ;done
+rm ~/www/artifacts/* -rf
+cp -al artifacts/* ~/www/artifacts
+#for f in $(find artifacts/ -type d);do mkdir -p ~/www/$f; files=$(ls -Fd $f/* | grep -v "/$"| sed 's/[*@]$//');ln $files ~/www/$f/ ;done
 
