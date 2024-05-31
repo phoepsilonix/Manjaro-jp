f6fc841aa2a8add3620238294cbd2d917cd58b62
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2024-05-31 11:37:58 +0900

    iso-profile shared

--- keychain.sh
+++ keychain.sh
@@ -6,6 +6,14 @@ ssh_passphrase=~/.ssh/ssh-passphrase.gpg
 ####### expectによる、履歴を残さない
 export HISTIGNORE="expect*";
 
+function call_keychain()
+{
+
+###### expectによる、履歴を残さない
+export HISTIGNORE="expect*";
+
+gpg_pass=~/.ssh/gpg-passphrase
+ssh_pass=~/.ssh/ssh-passphrase.gpg
 ###### スクリプト終了時には、keychainをクリアして、ssh-agentを停止する。
 trap "
 keychain -k mine
@@ -13,21 +21,30 @@ keychain --clear
 " EXIT
 
 ###### sshのパスフレーズを復号化する
-PASSPHRASE=$(gpg --passphrase-file $gpg_passphrase --batch --pinentry-mode=loopback -dq $ssh_passphrase)
-gpg -dq ~/.ssh/pass.gpg | sudo -S pwd
+PASSPHRASE=$(gpg --passphrase-file $gpg_pass --batch --pinentry-mode=loopback -dq $ssh_pass)
+KEY_FILENAME=$1
+
+#spawn ssh-add $HOME/.ssh/$KEY_FILENAME
+#spawn keychain --agents ssh --eval $KEY_FILENAME
 
 ###### expectでパスフレーズ入力を自動化
-expect << EOF
-  spawn keychain --agents ssh --eval id_ed25519_3 id_ed25519
-  expect "* passphrase *:" {
-        send "$PASSPHRASE\r"
-  }
+expect <<EOF
+  spawn keychain --agents ssh --eval $KEY_FILENAME
   expect {
-    "denied" { exit 1 }
-    eof { exit 0 }
-  }
+    "* passphrase *:" {
+      send "$PASSPHRASE\r"
+      exp_continue
+    }
+  "denied" { exit 1 }
+  eof { exit 0 }
+}
 EOF
+eval `keychain --eval $KEY_FILENAME 2>/dev/null`
+}
+
+call_keychain id_ed25519_3
+call_keychain id_ed25519-2
+call_keychain id_aur
+call_keychain id_ed25519
 
 ###### このスクリプト内で、有効化させる。
-eval `keychain --agents ssh --eval id_ed25519_3 id_ed25519 2>/dev/null`
-
