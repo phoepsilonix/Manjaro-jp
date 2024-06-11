ec4cfa9ded80c5e687777a10f9035df2851df009
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2024-06-11 17:03:06 +0900

    hardlink.sh

--- hardlink.sh
+++ hardlink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm ~/www/artifacts/* -rf
+mkdir -p ~/www/artifacts/
 cp -al artifacts/* ~/www/artifacts
 #for f in $(find artifacts/ -type d);do mkdir -p ~/www/$f; files=$(ls -Fd $f/* | grep -v "/$"| sed 's/[*@]$//');ln $files ~/www/$f/ ;done
 
