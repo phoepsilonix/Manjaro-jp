13693d56d1dbc6cfa6bdc06894f7e0c79a506c5f
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2025-01-24 19:14:13 +0900

    line message api

--- line-notify.sh
+++ line-notify.sh
@@ -1,9 +1,27 @@
 #! /bin/sh
 
-P=$@
-token=$(cat ~/.ssh/line-token)
-
-curl -X POST -H "Authorization: Bearer $token" -F message="$P" https://notify-api.line.me/api/notify
-
+P="$@"
+token="$(cat ~/.ssh/.line-token)"
+#url="https://api.line.me/v2/bot/message/broadcast"
+url="https://api.line.me/v2/bot/message/push"
+user="$(cat ~/.ssh/.line-user)"
+MSG=$(cat <<EOF
+{
+     "to": "${user}",
+	 "messages": [
+        {
+            "type":"text",
+            "text":"${P}"
+        }
+    ]
+}
+EOF
+)
+echo $MSG
+curl -v -X POST $url \
+-H 'Content-Type: application/json' \
+-H "Authorization: Bearer $token" \
+-d "$MSG"
 exit 0
 
+#-H 'X-Line-Retry-Key: {UUID}' \
